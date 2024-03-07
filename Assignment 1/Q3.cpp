7bbc78a476eccaad8dc6accfcfc812d63c9427c4
Amit-Raskar <amitraskar09@gmail.com>
2024-03-07 16:42:05 +0530

    C++ Assignment 1

---
+++ Assignment 1/Q3.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+
+class Student
+{
+    private:
+        int roll_no;
+        string name;
+        int marks;
+    public:
+        void initStudent()
+        {
+            roll_no = 69;
+            name = "Amit";
+            marks = 96;
+            cout<<"Details of student are:" << endl << "Roll No. -" << roll_no << endl << "Name -" << name << endl << "Marks -" << marks << endl;
+        }
+        void printStudentOnConsole()
+        {
+            cout<<"Details of student are:" << endl << "Roll No. -" << roll_no << endl << "Name -" << name << endl << "Marks -" << marks << endl;
+        }
+        void acceptStudentFromConsole()
+        {
+            cout<<"Enter Roll No., Name and Marks of a student:"<<endl;
+            cin>> roll_no >> name >> marks;
+        }
+
+};
+int main()
+{
+    int choice;
+    Student s1;
+    do
+    {
+        cout<<"Please select: 0.Exit" << endl << "1.Initializing automatically" << endl << "2.Print student details" << endl << "3.Fill students details" << endl;
+        cin>> choice;
+        switch (choice)
+        {
+            case 1:
+                s1.initStudent();
+                break;
+            case 2:
+                s1.printStudentOnConsole();
+                break;
+            case 3:
+                s1.acceptStudentFromConsole();
+                break;
+        }
+    } while (choice != 0);
+    return 0;    
+}
